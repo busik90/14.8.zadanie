edd823b2d2e4415666bb8513fd27df95548df427
Pawel Bus <pawelbus.dev@outlook.com>
2018-07-06 13:02:29 +0200

    Fix request url

--- components/App.js
+++ components/App.js
@@ -22,7 +22,7 @@ App = React.createClass({
   },
 
   getGif: function(searchingText, callback) {  // 1.
-    var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  // 2.
+    var url = 'http://api.giphy.com/v1/gifs/search?q=' + searchingText + '&api_key=dc6zaTOxFJmzC&limit=1';  // 2.
     var xhr = new XMLHttpRequest();  // 3.
 
     xhr.open('GET', url);
